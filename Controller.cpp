1d855f891ef47f3f657d431cbc6cf6fb411470fc
Anthony Garcia Roman <100794440+Aerodite@users.noreply.github.com>
2023-01-26 11:13:29 -0800

    Basic C++ classes and libraries that would (should) allow any GenericHID controller class usage on the robot.

---
+++ Controller.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Anthony Garcia Roman on 26/1/2023.
+//
+
+#include "Controller.h"
+frc::PWMVictorSPX m_leftMotor{Constants::kLeftMotorPort};
+frc::PWMVictorSPX m_rightMotor{Constants::kRightMotorPort};
+frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
+frc::GenericHID m_stick{Constants::kJoystickPort};
+
+m_robotDrive.ArcadeDrive(-m_stick.GetRawAxis(0), m_stick.GetRawAxis(1));
